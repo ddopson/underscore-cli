a0df8dc0b6514432c0ba932f9f6ec4bdd278afda
Dave Dopson <ddopson@zynga.com>
2012-04-04 13:24:07 -0700

    add isArray, isObject, isNumber, isString

--- test.sh
+++ test.sh
@@ -1,9 +1,13 @@
-underscore examples | grep '#' | grep -v '|' | cut -d" " -f2- | while read line; do 
+#!/bin/bash
+set -e
+
+jshint bin/underscore || exit
+bin/underscore examples | grep '#' | grep -v '|' | cut -d" " -f2- | perl -pe 's/underscore/bin\/underscore/' | while read line; do 
   echo "# $line"
   echo -n 'prints: '
   bash -c "$line"
   echo
-done > actual.txt
-underscore examples | perl -ne '/\|/ and $s=3; print unless $s-- > 0;' > expected.txt
+done | perl -pe 's/bin\/underscore/underscore/' > actual.txt
+bin/underscore examples | perl -ne '/\|/ and $s=3; print unless $s-- > 0;' > expected.txt
 diff -ur expected.txt actual.txt
 
