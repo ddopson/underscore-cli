703076be3e7c0fae692dd2c1d1ee25327899f150
Dave Dopson <ddopson@zynga.com>
2012-04-11 20:03:57 -0700

    add input/output formats, refactor help system, change examples format, several bugfixes

--- test.sh
+++ test.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
-set -e
 
 jshint bin/underscore || exit
-bin/underscore examples | grep '#' | grep -v '|' | cut -d" " -f2- | perl -pe 's/underscore/bin\/underscore/' | while read line; do 
-  echo "# $line"
-  echo -n 'prints: '
-  bash -c "$line"
+bin/underscore examples | grep -v '#' | grep . | perl -pe 's/underscore/bin\/underscore/' | while read line; do 
+  echo "$line"
+  bash -c "$line" | perl -pe 's/^/# /g'
   echo
 done | perl -pe 's/bin\/underscore/underscore/' > actual.txt
-bin/underscore examples | perl -ne '/\|/ and $s=3; print unless $s-- > 0;' > expected.txt
+bin/underscore examples > expected.txt
 diff -ur expected.txt actual.txt
 
