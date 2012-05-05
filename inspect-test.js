f94863f3dec80567fd698f399e876b49c8b46eaf
Dave Dopson <ddopson@gmail.com>
2012-05-04 19:07:23 -0700

    temp_commit - add partially completed work on the new formatter

---
+++ inspect-test.js
@@ -0,0 +1,42 @@
+#!/usr/bin/env node
+
+var util = require('util');
+var util2 = require('./lib/util');
+
+var obj = {
+  num: 9,
+  
+  str1: 'Hello World',
+  str2: (v='mystring', v.prop1=8, v[3]=43, v),
+
+  o1: {},
+  o2: (v={a: 9}, v[3]=4, v),
+
+  date: new Date(),
+  date2: (v=new Date(), v.prop1=8, v.prop2=9, v),
+
+
+
+  fn1: function () { },
+  fn2: function fn_name () { },
+  fn3: (v=function hi(a,b,c){return 4;}, v.prop1=1, v.prop2=2, v),
+
+  re1: /^78/,
+  re2: (v=/^78/,v.bar=2,v),
+
+
+  ar1: [1, 2, 3, 4],
+  ar2: (v=[1, 2, 3, 4], v.bar=33, v[6]=8,v),
+
+  longstr: 'aoseuthaoesnuhaosenthuasoenthuasoenthuasoenthuasoenthuasnoethuasnoethuasonethuasnoethusanoethiasnoethuasonethuasoenhuasnoethuasnoethuasonethusanoethusnaoethuasnoethuiasnoeidaosneutdhaoesntuhaoesnthuasonehuasnoethuaosentuhasoenthuaosnethuasoenthuasoenthuasoentuhasnoethuasnoehuasnoethuasnoethuasonethuasnotehuasnotehuasnoethuasonethuasoentuhaoseu',
+  deep: {a: {b: {c: {}}}},
+};
+
+console.log("\n\nJSON:");
+console.log(JSON.stringify(obj, null, ' '));
+console.log("\n\nINSPECT:");
+console.log(util.inspect(obj, false, 1, true));
+console.log("\n\nINSPECT2:");
+console.log(util2.inspect(obj, false, -1));
+
+
